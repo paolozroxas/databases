5277b4658eec40b45d05bf8de6a9ff7995c5b25a
paolozroxas <paolo.z.roxas@gmail.com>
2018-02-09 20:09:04 -0800

    wrote database schema, made diagram for ondras.zarovi.cz/sql/demo/

--- server/schema.sql
+++ server/schema.sql
@@ -3,13 +3,23 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  body VARCHAR(512),
+  room_id INT
 );
 
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(32)
+)
+
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(32)
+)
+
 /* Create other tables and define schemas for them here! */
-
-
-
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
